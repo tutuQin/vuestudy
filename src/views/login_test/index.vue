ab7ee12e0b81c2911e24a5a711c2ce8ba66e71cf
allen <allen@qqwweerr.local>
2019-06-26 22:51:34 +0800

    ad

--- src/views/login_test/index.vue
+++ src/views/login_test/index.vue
@@ -1,24 +1,79 @@
 <template>
   <div class="login_test">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login_test-form" autocomplete="on" label-position="left">
-      <div class="title-container">
-        <h3 class="title">登陆</h3>
+    <div class="title-container">
+      <h3 class="title">
+        User Login
+      </h3>
+    </div>
+    <div class="formbox">
+      <el-form
+        ref="form"
+        :model="form"
+      >
+        <el-form-item>
+          <span class="svg-container">
+            <svg-icon icon-class="user" />
+          </span>
+          <el-input 
+            v-model="form.name" 
+            placeholder="请输入账号"
+            name="username"
+          />
+        </el-form-item>
+        <el-tooltip
+          v-model="capsTooltip"
+          content="Caps lock is On"
+          placement="right"
+          manual
+        >
+          <el-form-item prop="password">
+            <span class="svg-container">
+              <svg-icon icon-class="password" />
+            </span>
+            <el-input 
+              v-model="form.password" 
+              placeholder="请输入密码" 
+              show-password
+            />
+          </el-form-item>
+        </el-tooltip>
+        <el-row>
+          <el-button 
+            type="primary"
+            style="width:100% ; padding:16px 0;"
+            @click="dayin()"
+          >
+            确定
+          </el-button>
+        </el-row>
+      </el-form>
+      <div>
+        <p
+          class="zhxx"
+          style="color:#fff;float:left"
+        >
+          账号：abcd     密码：12345
+          <br>
+          账号：abcd     密码：12345
+        </p>
+        <span
+          class="qqwechat"
+          style="float:right;padding-top:16px;"
+        >
+          <el-row>
+            <el-button 
+              type="success"
+              style="width:160px ; padding:12px 0;"
+              @click="1"
+            >
+              其他账户登陆
+            </el-button>
+          </el-row>
+        </span>      
       </div>
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          autocomplete="on"
-        />
-      </el-form-item>
-    </el-form>
+      <br>
+      <!-- <div style="max-width:500px ;width:80%; padding:14px 0;color:#fff;font-size:16px;padding-top:12px;" >{{form.name}}{{form.password}} </div> -->
+    </div>
   </div>
 </template>
 
@@ -28,56 +83,109 @@ export default {
   name: 'LoginTest',
 
   components: {},
-  props: [''],
   data() {
     return {
-
+      form:{
+      name:'',
+      password:''
+      },
+      capsTooltip: true,
     }
   },
 
-  computed: {},
+  computed: {
+  },
 
   watch: {},
 
-  beforeMount() {},
+  beforeMount(){},
 
-  mounted() {},
+  mounted(){},
+  
 
-  methods: {}
-
+  methods: {
+    dayin(){
+   } 
+  }
 }
 
 </script>
+/* 全局css */
+<style lang='scss'>
+    .el-input{
+       width:80%;
+      line-height: 60px;
+       display: inline-block;
+      
+       input{
+          background: #283442;
+          border: 0px;
+          -webkit-appearance: none;
+          border-radius: 0px;
+          // padding: 24px 0px 12px 15px;
+          color: #ffffff;
+          line-height: 60px;
+          caret-color: #899aa3;
+        }
+
+    }
+     .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #899aa3;
+      }
+   .svg-container {
+    // padding: 6px 5px 6px 15px;
+    padding-left: 16px;
+ 
+    vertical-align: middle;
+    width:15%;
+    display: inline-block;
+  }
+</style>
+
+
+
+
 <style lang='scss' scoped>
-    $bg:#2d3a4b;
-    $light_gray:#fff;
-    $cursor: #fff;
+   
+
     .login_test{
-      min-height: 100%;
+      height: 100vh;
       width: 100%;
       background-color: #2d3a4b;
       overflow: hidden;
 
-       .login_test-form {
-          position: relative;
-          width: 520px;
-          max-width: 100%;
-          padding: 160px 35px 0;
-          margin: 0 auto;
-          overflow: hidden;
-        }
     }
+    .formbox{
+          width: 80%;
+          max-width: 480px;
+          margin: 0 auto;
 
+
+        }
   .title-container {
-    position: relative;
+      margin-top: 140px;
 
     .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
+      font-size:28px;
+      color:#fff;
+      margin: 40px auto 40px auto;
       text-align: center;
-      font-weight: bold;
     }
+ 
+    .show{
+      font-size: 16px;
+      color: #fff;
+    
+    }
+   @media only screen and (max-width: 470px) {
+    .qqwechat {
+      display: none;
+    }
+  }
+
   }
 
 </style>
